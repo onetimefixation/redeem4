0e036ed2ac4bc58f09d2524a1f1eee089f84813f
astro <onetimefixation@gmail.com>
2021-05-03 23:37:26 -0400

    version1

---
+++ jsFile.js
@@ -0,0 +1,11 @@
+/* document.getElementById('redeem').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.getElementById('button-cancel').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+}); */
